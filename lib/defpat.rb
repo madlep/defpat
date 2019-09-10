6442ca9cda7b592ea0e95fb5c76303bc3e120d17
Julian Doherty (madlep) <madlep@madlep.com>
2019-09-10 22:15:01 +1000

    basic concept working

--- lib/defpat.rb
+++ lib/defpat.rb
@@ -1,6 +1,68 @@
 require "defpat/version"
 
 module Defpat
-  class Error < StandardError; end
-  # Your code goes here...
+  class NoPatternMatchError < StandardError; end
+
+  class << self
+    def included(base)
+      base.extend(ClassMethods)
+    end
+  end
+
+  module ClassMethods
+    def defpat(m, *matching_args, &block)
+      head = heads[m]
+      unless head
+        head = MethodHead.new(m)
+        heads[m] = head
+        define_method(m) do |*args|
+          clause = self.class.heads[m].matching_clause(args)
+          if clause
+            clause.call(self, args)
+          else
+            raise NoPatternMatchError, [args]
+          end
+        end
+      end
+      head.add_clause(matching_args, block)
+    end
+
+    def heads
+      @heads ||= {}
+    end
+  end
+
+  class MethodHead
+    def initialize(name)
+      @name = name
+      @clauses = []
+    end
+
+    def add_clause(args, block)
+      @clauses << Clause.new(args, block)
+    end
+
+    def matching_clause(args)
+      @clauses.find{|c| c.match?(args)}
+    end
+  end
+
+  class Clause
+    def initialize(args, block)
+      @matching_args = args
+      @block = block
+    end
+
+    def match?(other_args)
+      if @matching_args.length != other_args.length
+        return false
+      else
+        @matching_args.zip(other_args).all?{|a,b| a === b}
+      end
+    end
+
+    def call(obj, args)
+      obj.instance_exec(*args, &@block)
+    end
+  end
 end
--- test/defpat_test.rb
+++ test/defpat_test.rb
@@ -1,11 +1,68 @@
 require "test_helper"
+class Fib
+  include Defpat
 
-class DefpatTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::Defpat::VERSION
+  defpat :fib, 0 do |_n|
+    0
   end
 
-  def test_it_does_something_useful
-    assert false
+  defpat :fib, 1 do |_n|
+    1
+  end
+
+  defpat :fib, Object do |n|
+    fib(n - 1) + fib(n - 2)
+  end
+end
+
+describe Fib do
+  before do
+    @f = Fib.new
+  end
+
+  it "allows fibonacci sequence to be defined with pattern matches" do
+    _(@f.fib(0)).must_equal 0
+    _(@f.fib(1)).must_equal 1
+    _(@f.fib(2)).must_equal 1
+    _(@f.fib(3)).must_equal 2
+    _(@f.fib(14)).must_equal 377
+  end
+end
+
+class FizzBuzz
+  include Defpat
+
+  defpat :fizzbuzz, ->(x){x % 3 == 0 && x % 5 == 0} do |_n|
+    "FizzBuzz"
+  end
+
+  defpat :fizzbuzz, ->(x){x % 3 == 0} do |_n|
+    "Fizz"
+  end
+
+  defpat :fizzbuzz, ->(x){x % 5 == 0} do |_n|
+    "Buzz"
+  end
+
+  defpat :fizzbuzz, Integer do |n|
+    n
+  end
+end
+
+describe FizzBuzz do
+  before do
+    @fb = FizzBuzz.new
+  end
+
+  it "allows fizzbuzz to be defined with pattern matches" do
+    _(@fb.fizzbuzz(1)).must_equal 1
+    _(@fb.fizzbuzz(2)).must_equal 2
+    _(@fb.fizzbuzz(3)).must_equal "Fizz"
+    _(@fb.fizzbuzz(5)).must_equal "Buzz"
+    _(@fb.fizzbuzz(15)).must_equal "FizzBuzz"
+  end
+
+  it "can't be called with a non-matching pattern" do
+    _(@fb.fizzbuzz("not a number", "or this")).must_equal :foobar
   end
 end
